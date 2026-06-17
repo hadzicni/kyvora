c6c56dade7608bfe7e2f24434aba7d837a488efe
Nikola Hadzic <nikolahadzic7@icloud.com>
2026-06-17 16:34:59 +0200

    docs(api): use runtime version in OpenAPI metadata

--- apps/api/src/main/java/dev/kyvora/api/config/openapi/OpenApiConfig.java
+++ apps/api/src/main/java/dev/kyvora/api/config/openapi/OpenApiConfig.java
@@ -1,22 +1,22 @@
 package dev.kyvora.api.config.openapi;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import dev.kyvora.api.status.service.StatusService;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(
-		title = "Kyvora API",
-		description = "Open-source Homelab Control Plane API",
-		version = "0.1.0"))
-@SecurityScheme(
-		name = OpenApiConfig.BEARER_AUTH_SCHEME,
-		type = SecuritySchemeType.HTTP,
-		scheme = "bearer",
-		bearerFormat = "JWT")
 public class OpenApiConfig {
 
 	public static final String BEARER_AUTH_SCHEME = "bearerAuth";
+
+	@Bean
+	public OpenAPI openAPI(StatusService statusService) {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Kyvora API")
+						.description("Open-source Homelab Control Plane API")
+						.version(statusService.status().version()));
+	}
 }
