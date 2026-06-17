e913aa03419a90a0dbad1f9ffce4ceade3e0728e
Nikola Hadzic <nikolahadzic7@icloud.com>
2026-06-17 16:38:44 +0200

    feat(openapi): configure JWT bearer auth for Swagger UI

--- apps/api/src/main/java/dev/kyvora/api/config/openapi/OpenApiConfig.java
+++ apps/api/src/main/java/dev/kyvora/api/config/openapi/OpenApiConfig.java
@@ -2,7 +2,10 @@ package dev.kyvora.api.config.openapi;
 
 import dev.kyvora.api.status.service.StatusService;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +20,15 @@ public class OpenApiConfig {
 				.info(new Info()
 						.title("Kyvora API")
 						.description("Open-source Homelab Control Plane API")
-						.version(statusService.status().version()));
+						.version(statusService.status().version()))
+				.components(new Components()
+						.addSecuritySchemes(BEARER_AUTH_SCHEME,
+								new SecurityScheme()
+										.name(BEARER_AUTH_SCHEME)
+										.type(SecurityScheme.Type.HTTP)
+										.scheme("bearer")
+										.bearerFormat("JWT")))
+				.addSecurityItem(new SecurityRequirement()
+						.addList(BEARER_AUTH_SCHEME));
 	}
 }
